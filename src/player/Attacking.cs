90a7b933c2f3f226881973b7c4b5ac05a3f7a5fd
Oreboat <111535650+Oreboat@users.noreply.github.com>
2024-03-19 15:12:51 -0500

    Attacking Beginning to be implemented

--- src/player/Attacking.cs
+++ src/player/Attacking.cs
@@ -3,4 +3,16 @@ using System;
 
 public partial class Attacking : Node
 {
+    bool enemy_in_range = false;
+    void _on_area_3d_body_entered(Node3D body)
+    {
+        
+    }
+    public override void _Input(InputEvent @event)
+    {
+        if(@event.IsActionPressed("Attack"))
+        {
+            GD.Print("Attacking");
+        }
+    }
 }
--- src/player/Player.cs
+++ src/player/Player.cs
@@ -7,17 +7,21 @@ public partial class Player : CharacterBody3D
     public Health Health;
     public Moving moving;
     public Sprite2D playerSprite;
+    public Node3D Pivot;
 
     public override void _Ready()
     {
         Global.Target = this;
         Global.Player = this;
+        Pivot = GetNode("Pivot") as Node3D;
     }
     public override void _Input(InputEvent @event)
     {
         float x = Input.GetActionStrength("Move_Right") - Input.GetActionStrength("Move_Left");
         float y = Input.GetActionStrength("Move_Down") - Input.GetActionStrength("Move_Up");
         Vector2 input = new Vector2(x,y);
+        if(input != Vector2.Zero)
+            Pivot.Rotation = new Vector3(0,Mathf.Atan2(-y,x),0);
         InputVector = input.Normalized();
         
     }
